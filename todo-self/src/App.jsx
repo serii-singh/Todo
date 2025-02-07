0d0ef2ba0fd0a2947f6784f6fce5ad467def0811
Serene Singh <iamsereneisngh@gmail.com>
2025-02-07 14:14:56 +0530

    input-added

--- todo-self/src/App.jsx
+++ todo-self/src/App.jsx
@@ -8,9 +8,10 @@ function App() {
  
 
  const addTask =() =>{ 
-  const newTask = prompt("enter a task ");
+  // const newTask = prompt("enter a task ");
+  const newTask = task[task.length - 1]
   if(newTask){
-    setTask([...task,newTask])
+    setTask([...task.slice(0, task.length - 1), newTask, ""])
   }
  }
 const removeTask = () =>{
@@ -23,6 +24,15 @@ const removeTask = () =>{
 
 }
 
+const handleChange = (e) =>{
+  const newInput = e.target.value;
+  
+  setTask((prevTask) => {
+    const updatedTasks = [...prevTask]; // Create a copy of the array
+    updatedTasks[updatedTasks.length - 1] = newInput; // Update the last task
+    return updatedTasks; // Return the new array
+  });
+}
   return (
     <div className='border border-violet-100'>
      <div className="text-center text-4xl font-bold shadow-lg bg-slate-400 w-full text-blue-50 p-3">Todo-App</div>
@@ -32,6 +42,7 @@ const removeTask = () =>{
       className='text-4xl p-4 text-justify bg-pink-500 text-white rounded-md shadow-md hover:bg-violet-600'>
         Add
       </button>
+      
       <button 
       onClick={removeTask}
       className='text-4xl p-4 text-justify bg-pink-500 text-white rounded-md shadow-md hover:bg-violet-600'>Delete</button>
@@ -39,6 +50,15 @@ const removeTask = () =>{
       </div>
 
       <div className='render-list bg-slate-700 h-1/2 overflow-scroll mx-14  '>
+   
+      <input
+            className="p-4 text-2xl border-2 rounded-md bg-slate-300"
+            type="text"
+            value={task[task.length - 1] || ""} // Display the current input value
+            onChange={handleChange} // Update input value in the task array
+            placeholder="Type here"
+          />
+
         <ul>
           {task.length===0 ? (<li className='text-2xl text-white text-center p-4'>No Tasks Available</li>) : (
             task.map((task,index)=>(<li key={index} className='mx-2 px-2 text-2xl text-white py-2'>{task}</li>))
